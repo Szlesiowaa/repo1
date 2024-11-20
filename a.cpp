b26e0341ed4264dc78ac97bdc54825a75b39a726
jklonowski <jklonowski@mat.umk.pl>
2024-11-20 16:36:52 +0100

    galaz b

--- a.cpp
+++ a.cpp
@@ -4,5 +4,9 @@ int main(){
 //aaaaaaaaaaaaaaaa
 //druga linia aaaaaaaaaaa
 //repo1
+//
+//galaz g2
+//2
+//
 return 0;
 }
--- b.cpp
+++ b.cpp
@@ -3,6 +3,7 @@
 int main(){
 
 printf("b");
+printf("wszystko git dziala galaz b");
 
 return 0;
 }
